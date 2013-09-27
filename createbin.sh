b8cee64e95518bf37d95ccc2e5436a74aeaee279
Karel Lenc <karel@lenc.info>
2013-09-27 19:31:41 +0200

    Fixed the createbin script. Maven somehow changes the contents of guava jar file.

--- createbin.sh
+++ createbin.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-mvn package && scp scp ./target/ckvs-*-bin.tar.gz cassandra@tarski:~/ckvs-latest-bin.tar.gz
+tar cvzf target/ckvs-0.0.1-bin.tar.gz target/ckvs-0.0.1-jar-with-dependencies.jar lib/guava-14.0.1.jar && scp ./target/ckvs-*-bin.tar.gz cassandra@tarski:~/ckvs-latest-bin.tar.gz
 
