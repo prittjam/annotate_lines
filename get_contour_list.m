b9428303d71366e2b2b22a462062f37097a9a04b
James Pritts <prittjam@cmp.felk.cvut.cz>
2017-09-05 12:47:37 +0200

    changes

--- get_contour_list.m
+++ get_contour_list.m
@@ -1,6 +1,5 @@
 function [contour_list,par_pair,perp_pair] = ...
         get_contour_list(img)
-
     cid_cache = CASS.CidCache(img.cid);
     pth = pwd;
     
@@ -12,25 +11,25 @@ function [contour_list,par_pair,perp_pair] = ...
     cid_cache.add_dependency('perpendicular_lines',[], ...
                              'parents','contours'); 
 
-    contour_list = cid_cache.get('dr','contours'); 
+    contour_list = cid_cache.get('annotations','contour_list'); 
 
     if isempty(contour_list)
         [E,o] = edgesDetect(img.data,model);
         cd(pth);
         pts = DL.segment_contours(E);
         C = cmp_splitapply(@(x) { x },[pts(:).x],[pts(:).G]);
-        sz = cmp_splitapply(@(x)  numel(x) ,[pts(:).x],[pts(:).G]);
-        ind = sz > 20;
-        num_ind = sum(ind);
-        sC = C(ind);
-        cid_cache.put('dr','contours',sC);
+        sz = cmp_splitapply(@(x) numel(x),[pts(:).x],[pts(:).G]);
+        num_ind = numel(sz);
         l = zeros(3,num_ind);
-        for k = 1:numel(sC)
-            l(:,k) = LINE.fit(sC{k});
+        for k = 1:numel(C)
+            l(:,k) = LINE.fit(C{k});
         end
         contour_list = struct('C', C, ...
                               'l', mat2cell(l,3,ones(1,size(l,2))));
+        cid_cache.put('annotations','contour_list', contour_list);
     end
+
+    cd(pth);
 
     par_pair = ...
         cid_cache.get('annotations','parallel_lines');
@@ -38,21 +37,25 @@ function [contour_list,par_pair,perp_pair] = ...
     perp_pair = ...
         cid_cache.get('annotations','perpendicular_lines');
 
+
+    par_pair = [];
+    perp_pair = [];
     if isempty(par_pair)
         l = [contour_list(:).l];
         c = abs(l(1:2,:)'*l(1:2,:));
         c(c>1) = 1;
         theta = acos(c)*180/pi;
         ltri = itril([size(l,2) size(l,2)],-1);
-        par_ind = theta(ltri) < 5; 
-        inl_ind = ltri(find(par_ind));
-        [ii,jj] = ind2sub([size(l,2) size(l,2)],inl_ind);
+    
+        par_ind = find(theta(ltri) < 5);
+        par_inl_ind = ltri(par_ind);
+        [ii,jj] = ind2sub([size(l,2) size(l,2)],par_inl_ind);
         [~,sind] = sort(mean([sz(ii);sz(jj)],1),'descend');
         par_pair = [ii(sind) jj(sind)]';
         
         perp_ind = theta(ltri) > 85; 
-        inl_ind = ltri(find(perp_ind));
-        [ii2,jj2] = ind2sub([size(l,2) size(l,2)],inl_ind);
+        perp_inl_ind = ltri(find(perp_ind));
+        [ii2,jj2] = ind2sub([size(l,2) size(l,2)],perp_inl_ind);
         [~,sind] = sort(mean([sz(ii2);sz(jj2)],1),'descend');
         perp_pair = [ii2(sind) jj2(sind)]';
 
@@ -64,15 +67,3 @@ function [contour_list,par_pair,perp_pair] = ...
     end
     par_pair = par_pair;
     perp_pair = perp_pair;
-    
-    keyboard;
-    
-    %        itril()
-
-    %        set(uistate.handles.img,'HitTest','on');
-    %        set(uistate.handles.img,'ButtonDownFcn',@image_click_callback);
-    %        uistate.file_name = file_name;
-    %        uistate.path = path;
-    %        uistate.outlier = struct('h',[],'select',false);
-    %        uistate.ignore = struct('h',[],'select',false);
-    %        uistate.plane_list = cell(1,0);
