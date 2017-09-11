e3318b4b52e4e83d067a230c898e404b00d8f4f7
ivashmak <ivashmak@fel.cvut.cz>
2017-09-11 13:51:04 +0200

    changes

--- annotate_lines.m
+++ annotate_lines.m
@@ -397,11 +397,7 @@ plot(uistate.contour_list(uistate.par_cspond(uistate.par_count).cspond(1)).C(1,:
 plot(uistate.contour_list(uistate.perp_cspond(uistate.perp_count).cspond(2)).C(1,:),...
     uistate.contour_list(uistate.perp_cspond(uistate.perp_count).cspond(2)).C(2,:),...
     'Linewidth',4,'Color','green');
-% keyboard
-% for j = 1:numel(uistate.contour_list)
-%     plot(uistate.contour_list(j).C(1,:), uistate.contour_list(j).C(2,:), 'LineWidth', 2, 'Color', [0 1 0]);
-% end
-  
+
 hold off                   
    
 function [start_par_count, start_perp_count] = find_unlabeled_lines(uistate)
